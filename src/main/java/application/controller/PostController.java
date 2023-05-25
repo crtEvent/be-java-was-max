ad010863ac651b046ad66dc17101d7acce458a0e
crtEvent <crtevn@gmail.com>
2023-05-26 07:47:57 +0900

    [7단계] 글쓰기 페이지로 이동하는 기능 추가

---
+++ src/main/java/application/controller/PostController.java
@@ -0,0 +1,42 @@
+package application.controller;
+
+import application.model.User;
+import webserver.annotation.MyController;
+import webserver.annotation.MyRequestMapping;
+import webserver.http.factor.HttpMethod;
+import webserver.http.factor.session.SessionController;
+import webserver.http.request.HttpRequestMessage;
+import webserver.model.ModelAndView;
+
+@MyController
+public class PostController {
+
+	@MyRequestMapping(value = "/posts", method = HttpMethod.GET)
+	public ModelAndView list(HttpRequestMessage httpRequestMessage) {
+		ModelAndView modelAndView = new ModelAndView("/index.html");
+
+		User user = (User) SessionController.getSessionValue(httpRequestMessage, "LOGIN_SID");
+
+		if(user != null) {
+			modelAndView.addAttribute("userId", user.getUserId());
+			modelAndView.addAttribute("password", user.getPassword());
+		}
+
+		return modelAndView;
+	}
+
+	@MyRequestMapping(value = "/posts/write-page", method = HttpMethod.GET)
+	public ModelAndView writePage(HttpRequestMessage httpRequestMessage) {
+		ModelAndView modelAndView = new ModelAndView("/qna/form.html");
+
+		User user = (User) SessionController.getSessionValue(httpRequestMessage, "LOGIN_SID");
+
+		if(user != null) {
+			modelAndView.addAttribute("userId", user.getUserId());
+			modelAndView.addAttribute("password", user.getPassword());
+			return modelAndView;
+		}
+
+		return new ModelAndView("/index.html");
+	}
+}
